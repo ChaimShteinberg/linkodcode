5f03e482719be12551f4dc22045eab294bbef06f
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-07 12:12:13 +0300

    like style

--- client/src/components/Like.tsx
+++ client/src/components/Like.tsx
@@ -1,8 +1,10 @@
+import "../styles/like.css"
+
 function Like(props: { sum: number }) {
   return (
-    <section className=".like">
-      <img src="src/assets/icons/like.svg" alt="like" />
+    <section className="like">
       <p>{props.sum}</p>
+      <img src="src/assets/icons/like.svg" alt="like" />
     </section>
   );
 }
