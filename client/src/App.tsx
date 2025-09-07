ef663ebec757e1d278d2bc4222e23d8af971c697
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-07 14:56:42 +0300

    finish header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Post from "./components/Post.tsx";
+import Homepage from "./pages/Homepage.tsx";
 
 function App() {
   return (
     <>
+    <Homepage/>
       <Post
         imgUrl="../src/assets/images/IMG-20230206-WA0001.jpg"
         description="bla"
---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,16 @@
+import "../styles/homepage.css";
+import Header from "../components/Header";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+      <p>home</p>
+    </>
+  );
+}
+
+export default Homepage;
