0d38143b61cf59c42b4a1df6559fcef5c86d9cdc
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-08 10:07:16 +0300

    message if falied

--- server/src/dal/post.dal.js
+++ server/src/dal/post.dal.js
@@ -1,5 +1,6 @@
 import posts from "../assets/posts.json" with {type: "json"};
 
 export function readposts() {
-  return posts;
+  if (posts) return {msg: "The posts were successfully loaded", posts};
+  return {msg: "Error loading posts"}
 }
