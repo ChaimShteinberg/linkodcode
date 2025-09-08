e576675882bfbaa6051bd431b0323f8defed2354
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-08 15:06:06 +0300

    finish route to post page

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -2,19 +2,22 @@ import "../styles/post.css";
 import type { post } from "../interfaces/post.interface.ts";
 import Like from "./Like";
 import { serverPath } from "../api/posts.ts";
+import { Link } from "react-router";
 
 function Post(props: post) {
   return (
     <section className="post">
-      <img
-        src={`${serverPath}${props.imgUrl}`}
-        alt="post-img"
-        className="postImg"
-      />
-      <section className="description">
-        <h2>Description</h2>
-        <p>{props.description}</p>
-      </section>
+      <Link to={`/post/${props.id}`}>
+        <img
+          src={`${serverPath}${props.imgUrl}`}
+          alt="post-img"
+          className="postImg"
+        />
+        <section className="description">
+          <h2>Description</h2>
+          <p>{props.description}</p>
+        </section>
+      </Link>
       <footer>
         <section className="postData">
           <p>{props.name}</p>
