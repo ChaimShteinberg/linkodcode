e8a0cd1c0fa7ce5b9459667146e07c5da38124f7
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-08 12:06:17 +0300

    finish get post by id by server

--- server/src/routes/posts.route.js
+++ server/src/routes/posts.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { readAllPostsController } from "../controllers/posts.controller.js";
+import { getPostByIdController, readAllPostsController } from "../controllers/posts.controller.js";
 
 const postsRoute = express.Router();
 
 postsRoute.get("/getAll", readAllPostsController);
+
+postsRoute.get("/:id", getPostByIdController)
 
 export default postsRoute;
