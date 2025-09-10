ce77742550edcbfd34d2bebb000727a2e2c5fd18
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-10 09:33:31 +0300

    middleware auth

--- client/src/api/posts.ts
+++ client/src/api/posts.ts
@@ -2,7 +2,9 @@ export const serverPath = "http://localhost:1568";
 
 export async function getPosts() {
   try {
-    const res = await fetch(`${serverPath}/posts/getAll`);
+    const res = await fetch(`${serverPath}/posts/getAll`, {
+      credentials: "include",
+    });
     const result = await res.json();
     if (result.msg === "The posts were successfully loaded") {
       return result;
@@ -18,7 +20,9 @@ export async function getPostById(id: string | undefined) {
     if (!id) {
       throw new Error("id is not defined");
     }
-    const res = await fetch(`${serverPath}/posts/${id}`);
+    const res = await fetch(`${serverPath}/posts/${id}`, {
+      credentials: "include",
+    });
     const result = await res.json();
     if (result.msg === "The posts were successfully loaded") {
       return result;
@@ -34,6 +38,7 @@ export async function addPost(newPost: FormData) {
     const res = await fetch(`${serverPath}/posts/addpost`, {
       method: "POST",
       body: newPost,
+      credentials: "include",
     });
     const result = await res.json();
     if (result.msg === "The post was added successfully") {
